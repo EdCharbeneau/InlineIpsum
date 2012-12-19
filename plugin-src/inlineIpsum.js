bec203b96d372018f971349de5326e8f35ea3581
Ed Charbeneau <ed.charbeneau@gmail.com>
2012-12-18 20:24:58 -0500

    Basic jQuery module outlined

---
+++ plugin-src/inlineIpsum.js
@@ -0,0 +1,11 @@
+; (function ($, window, undefined) {
+    'use strict';
+
+    $.fn.inlineIpsum = function () {
+        //main
+        alert('passed');
+    };
+
+    $.fn.inlineIpsum.options = {};
+
+})(jQuery, window, document);
