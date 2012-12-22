2eae5e1e78f90a3f8decbd37f7d32d73b29ad27c
Ed Charbeneau <ed.charbeneau@gmail.com>
2012-12-21 21:10:34 -0500

    Testing fluent api

--- plugin-src/inlineIpsum.js
+++ plugin-src/inlineIpsum.js
@@ -1,9 +1,27 @@
 ; (function ($, window, undefined) {
     'use strict';
 
+    var IpsumWriter = function () {
+        var Cache;
+
+        this.p = function () {
+            this.Cache = '<p>Lorem Ipsum</p>';
+            return this;
+        }
+
+        this.write = function () {
+            return this.Cache;
+        }
+    };
+
     $.fn.inlineIpsum = function () {
         //main
-        alert('passed');
+        var Ipsum = new IpsumWriter();
+        var str = '@Html.Ipsum.p()';
+        var command = str.replace('@Html.','').concat('.write()');
+        console.log(command);
+        var result = eval(command);
+        alert(result);
     };
 
     $.fn.inlineIpsum.options = {};
