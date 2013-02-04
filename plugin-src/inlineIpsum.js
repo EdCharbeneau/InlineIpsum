da2e8784629877459e037d4f657487cbbb83fd3b
Ed Charbeneau <ed.charbeneau@gmail.com>
2013-02-04 16:40:15 -0500

    rewrote IpsumWriter

--- plugin-src/inlineIpsum.js
+++ plugin-src/inlineIpsum.js
@@ -1,27 +1,38 @@
-; (function ($, window, undefined) {
+//// Utility
+//if (typeof Object.create !== 'function') {
+//    Object.create = function (obj) {
+//        function F() { };
+//        F.prototype = obj;
+//        return new F();
+//    };
+//}
+; (function ($, window, document, undefined) {
     'use strict';
-
-    var IpsumWriter = function () {
-        var Cache;
-
-        this.p = function () {
-            this.Cache = '<p>Lorem Ipsum</p>';
+    
+    var IpsumWriter = {
+        cache: "",
+        p: function () {
+            this.cache += '<p>Lorem Ipsum</p>';
             return this;
-        }
+        },
 
-        this.write = function () {
-            return this.Cache;
+        h: function () {
+            this.cache += '<h1>Lorem Ipsum</h1>';
+            return this;
+        },
+
+        write: function () {
+            return this.cache;
         }
     };
 
     $.fn.inlineIpsum = function () {
         //main
-        var Ipsum = new IpsumWriter();
-        var str = '@Html.Ipsum.p()';
+        var Ipsum = Object.create(IpsumWriter);
+        var str = '@Html.Ipsum.p().h()';
         var command = str.replace('@Html.','').concat('.write()');
-        console.log(command);
+        console.log(eval(command));
         var result = eval(command);
-        alert(result);
     };
 
     $.fn.inlineIpsum.options = {};
