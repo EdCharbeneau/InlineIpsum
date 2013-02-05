ed165a0a185749320457d1a46c668be597ffb807
Ed Charbeneau <ed.charbeneau@gmail.com>
2013-02-05 14:14:39 -0500

    Not sure if this is necessary, removing for now.

--- plugin-src/inlineIpsum.js
+++ plugin-src/inlineIpsum.js
@@ -1,11 +1,4 @@
-//// Utility
-//if (typeof Object.create !== 'function') {
-//    Object.create = function (obj) {
-//        function F() { };
-//        F.prototype = obj;
-//        return new F();
-//    };
-//}
+
 ; (function ($, window, document, undefined) {
     'use strict';
 
