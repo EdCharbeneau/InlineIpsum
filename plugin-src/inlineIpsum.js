70194c52d013f858dec1b52cc1fd46edc50f2455
Ed Charbeneau <ed.charbeneau@gmail.com>
2013-02-05 14:10:41 -0500

    hn, p features are working

--- plugin-src/inlineIpsum.js
+++ plugin-src/inlineIpsum.js
@@ -11,25 +11,61 @@
 
     var IpsumWriter = {
         cache: "",
-        p: function () {
-            this.cache += '<p>Lorem Ipsum</p>';
+        tagBuilder: function (tag, text, attributes) {
+            attributes = $.extend({}, { text: text }, attributes);
+            return $(tag, attributes)[0].outerHTML;
+        },
+
+        p: function (attributes) {
+            var tag = "<p></p>";
+            var text = "Lorem Ipsum";
+            this.cache += this.tagBuilder(tag, text, attributes);
             return this;
         },
 
-        h1: function () {
-            var element =$("<h1></h1>", {
-                text: "Lorem Ipsum"
-            })[0].outerHTML;
-            this.cache += element;
+        h: function (level, attributes) {
+            var tag = "<h" + level + "></h" + level + ">";
+            var text = "Lorem Ipsum";
+            this.cache += this.tagBuilder(tag, text, attributes);
             return this;
         },
 
+        //helper methods
+        h1: function (attributes) {
+            this.h(1, attributes);
+            return this;
+        },
+       
+        h2: function (attributes) {
+            this.h(2, attributes);
+            return this;
+        },
+
+        h3: function (attributes) {
+            this.h(3, attributes);
+            return this;
+        },
+
+        h4: function (attributes) {
+            this.h(4, attributes);
+            return this;
+        },
+
+        h5: function (attributes) {
+            this.h(5, attributes);
+            return this;
+        },
+
+        h6: function (attributes) {
+            this.h(6, attributes);
+            return this;
+        },
         write: function () {
             return this.cache;
         }
     };
 
-    var justDoIt = function (str) {
+    var inline = function (str) {
         var Ipsum = Object.create(IpsumWriter);
         var command = str.replace('@Html.', '').concat('.write()');
         return eval(command);
@@ -38,11 +74,7 @@
     $.fn.inlineIpsum = function () {
         //main
         var Ipsum = Object.create(IpsumWriter);
-        //var str = '@Html.Ipsum.p().h()';        
-        //var command = str.replace('@Html.','').concat('.write()');
-        //console.log(eval(command));
-        //var result = eval(command);
-        return this.replaceText(/@Html.Ipsum()[^\s]+/gi, justDoIt);
+        return this.replaceText(/@html.Ipsum(\..*?\))*/gi, inline);
 
     };
 
