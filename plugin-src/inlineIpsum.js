0f288b12981b8b25afbd3e2bb4a925c098a8795a
Ed Charbeneau <ed.charbeneau@gmail.com>
2013-02-05 16:33:01 -0500

    Ipsum feature complete
    Time to refactor

--- plugin-src/inlineIpsum.js
+++ plugin-src/inlineIpsum.js
@@ -1,48 +1,58 @@
-
+/// <reference path="../javascripts/lorem.js" />
 ; (function ($, window, document, undefined) {
     'use strict';
 
     var IpsumWriter = {
 
         cache: "",
+
         addCache: function(item)
         {
             this.cache += item[0].outerHTML;
         },
+
         tagBuilder: function (tag, text, attributes) {
             attributes = $.extend({}, { text: text }, attributes);
             return $(tag, attributes);
         },
 
-        p: function (attributes) {
+        p: function (paragraphCount, sentenceCount, attributes) {
+            paragraphCount = paragraphCount || 1;
+            sentenceCount = sentenceCount || 5;
+            var lorem = new Lorem();
             var tag = "<p></p>";
-            var text = "Lorem Ipsum";
-            this.addCache(this.tagBuilder(tag, text, attributes));
+            for (var i = 0; i < paragraphCount; i++) {
+                this.addCache(this.tagBuilder(tag, lorem.createText(sentenceCount, Lorem.TYPE.SENTENCE), attributes));
+            }
             return this;
         },
 
-        h: function (level, attributes) {
+        h: function (level, wordCount, attributes) {
+            wordCount = wordCount || 2;
+            var lorem = new Lorem();
             var tag = "<h" + level + "></h" + level + ">";
             var text = "Lorem Ipsum";
-            this.addCache(this.tagBuilder(tag, text, attributes));
+            this.addCache(this.tagBuilder(tag, lorem.createText(wordCount, Lorem.TYPE.WORD), attributes));
             return this;
         },
 
-        listItem: function (tag, hasLink, attributes) {
-            var text = "Lorem Ipsum";
+        listItem: function (tag, wordCount, hasLink, attributes) {
+            wordCount = wordCount || 2;
+            var lorem = new Lorem();
             hasLink = hasLink || false;
-            var li = this.tagBuilder(tag, text, attributes);
+            var li = this.tagBuilder(tag, lorem.createText(wordCount, Lorem.TYPE.WORD), attributes);
             if (hasLink) {
                 li.wrapInner("<a href='#'></a>");
             }
             return li;
         },
 
-        list: function (outerTag, innerTag, count, hasLinks, outerAttributes, innerAttributes) {
-            var count = count || 5;
+        list: function (outerTag, innerTag, count, wordCount, hasLinks, outerAttributes, innerAttributes) {
+            wordCount = wordCount || 2;
+            count = count || 5;
             var outerElem = this.tagBuilder(outerTag, "", outerAttributes);
             for (var i = 0; i < count; i++) {
-                outerElem.append(this.listItem(innerTag, hasLinks, innerAttributes));
+                outerElem.append(this.listItem(innerTag, wordCount, hasLinks, innerAttributes));
             }
             this.addCache(outerElem);
         },
@@ -51,48 +61,48 @@
         },
 
         //helper methods
-        dl: function (count, hasLinks, olAttributes, liAttributes) {
-            this.list("<dl></dl>", "<dd></dd>", count, hasLinks, olAttributes, liAttributes);
+        dl: function (listCount, wordCount, hasLinks, olAttributes, liAttributes) {
+            this.list("<dl></dl>", "<dd></dd>", listCount, wordCount, hasLinks, olAttributes, liAttributes);
             return this;
         },
 
-        ol: function (count, hasLinks, olAttributes, liAttributes) {
-            this.list("<ol></ol>", "<li></li>", count, hasLinks, olAttributes, liAttributes);
+        ol: function (listCount, wordCount, hasLinks, olAttributes, liAttributes) {
+            this.list("<ol></ol>", "<li></li>", listCount, wordCount, hasLinks, olAttributes, liAttributes);
             return this;
         },
 
-        ul: function (count, hasLinks, ulAttributes, liAttributes) {
-            this.list("<ul></ul>", "<li></li>", count, hasLinks, ulAttributes, liAttributes);
+        ul: function (listCount, wordCount, hasLinks, ulAttributes, liAttributes) {
+            this.list("<ul></ul>", "<li></li>", listCount, wordCount, hasLinks, ulAttributes, liAttributes);
             return this;
         },
 
-        h1: function (attributes) {
-            this.h(1, attributes);
+        h1: function (wordCount, attributes) {
+            this.h(1, wordCount, attributes);
             return this;
         },
 
-        h2: function (attributes) {
-            this.h(2, attributes);
+        h2: function (wordCount, attributes) {
+            this.h(2, wordCount, attributes);
             return this;
         },
 
-        h3: function (attributes) {
-            this.h(3, attributes);
+        h3: function (wordCount, attributes) {
+            this.h(3, wordCount, attributes);
             return this;
         },
 
-        h4: function (attributes) {
-            this.h(4, attributes);
+        h4: function (wordCount, attributes) {
+            this.h(4, wordCount, attributes);
             return this;
         },
 
-        h5: function (attributes) {
-            this.h(5, attributes);
+        h5: function (wordCount, attributes) {
+            this.h(5, wordCount, attributes);
             return this;
         },
 
-        h6: function (attributes) {
-            this.h(6, attributes);
+        h6: function (wordCount, attributes) {
+            this.h(6, wordCount, attributes);
             return this;
         }
     };
