0b57991ffc065d757e73bbef94e1fb638ef04ce7
Ed Charbeneau <ed.charbeneau@gmail.com>
2013-02-06 12:22:26 -0500

    Added engine extensions

---
+++ plugin-src/extensions/extensions.md
@@ -0,0 +1,5 @@
+#Extensions:
+
+The inlneIpsum engine can be extended to include custom helpers.
+
+See the h7.ipsum.extension.js for an example.
--- plugin-src/extensions/h7.ipsumExtension.js
+++ plugin-src/extensions/h7.ipsumExtension.js
@@ -1,3 +1,11 @@
+//to add this extension 
+//  Reference this script
+//  <script src="h7.ipsumExtension.js"></script>
+//  Add the extension to the inlineIpsum settings
+//  $('body *').inlineIpsum({
+//            extension: MyEngine,
+//  });
+//  The helper method @Html.Ipsum.h7() is now enabled in your markup
 var MyEngine = {
         h7: function (wordCount, attributes) {
             this.h(7, wordCount, attributes);
