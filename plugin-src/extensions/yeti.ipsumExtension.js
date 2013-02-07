dd5f13b88a1edfa77bb7b74cefa077eeb9700e34
Ed Charbeneau <ed.charbeneau@gmail.com>
2013-02-06 21:12:21 -0500

    Added orbit helper

--- plugin-src/extensions/yeti.ipsumExtension.js
+++ plugin-src/extensions/yeti.ipsumExtension.js
@@ -35,5 +35,15 @@ var yetiIpsum =
         attributes = this._appendClass(attributes, "inline-list");
         this.ul(listCount, wordCount, true, attributes);
         return this;
+    },
+    orbit: function (id, width, height, imageCount) {
+        var colors = ["4086AA","91C3DC","87907D","AAB6A2", "555555"] 
+        var wrapper = $("<div></div>", { id: id });
+        for (var i = 0; i < imageCount; i++) {
+            //after 5 we'll use random colors, seriously who has this many slides in thier deck?
+            wrapper.append(this._image(width, height, "[img]", i < 4 ? colors[i] : Math.floor(Math.random() * 16777215).toString(16)))
+        }
+        this._addCache(wrapper);
+        return this;
     }
 }
